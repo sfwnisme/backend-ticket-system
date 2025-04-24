5f10fcea615fb1cc6d748cc995e6e8f3fa38da1d
sfwnisme <safymo424@gmail.com>
2025-04-25 02:25:02 +0300

    refactor(response.js): changed function name

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,6 +1,6 @@
 const { SUCCESS, ERROR } = require('../config/statusText.config')
 
-module.exports = (
+const formatApiResponse = (
   status = 500,
   statusText = ERROR,
   msg = 'internal server error',
@@ -11,3 +11,17 @@ module.exports = (
   }
   return { status, statusText, msg, data }
 }
+
+module.exports = { formatApiResponse }
+
+// const formatApiResponse = (
+//   status = 500,
+//   statusText = ERROR,
+//   msg = 'internal server error',
+//   data = null
+// ) => {
+//   if (statusText !== SUCCESS) {
+//     return { status, statusText, msg, error: data }
+//   }
+//   return { status, statusText, msg, data }
+// }
