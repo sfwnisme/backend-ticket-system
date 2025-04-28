4be2cca839e97835f1f762b582719a16584f80a6
sfwnisme <safymo424@gmail.com>
2025-04-29 01:27:03 +0300

    feat(ticket.routes.js): remove verifyToken from routes and place it at the top, and apply update validation

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -4,37 +4,34 @@ const controllers = require('../controllers/ticket.controllers');
 const verifyToken = require('../middlewares/verifyToken');
 const authorizedRole = require('../middlewares/authorizedRole');
 const userRoles = require('../config/userRoles.config');
-const { createTicketValidation, deleteTicketsValidation, singleTicketValidation } = require('../middlewares/ticketValidationSchema');
+const { createTicketValidation, deleteTicketsValidation, singleTicketValidation, updateTicketValidation } = require('../middlewares/ticketValidationSchema');
+
+router.use(verifyToken)
 
 router.route('/').get(
-  verifyToken,
   authorizedRole(userRoles.ADMIN, userRoles.MANAGER),
   controllers.getAllTickets
 ).delete(
-  verifyToken,
   authorizedRole(userRoles.ADMIN, userRoles.MANAGER),
   deleteTicketsValidation(),
   controllers.deleteTickets
 )
 
 router.route('/create').post(
-  verifyToken,
   authorizedRole(userRoles.ADMIN, userRoles.MANAGER),
   createTicketValidation(),
   controllers.createTicket
 )
 
 router.route('/:ticketId').get(
-  verifyToken,
   authorizedRole(userRoles.ADMIN, userRoles.MANAGER),
   singleTicketValidation(),
   controllers.getSingleTicket
 ).patch(
-  verifyToken,
   authorizedRole(userRoles.ADMIN, userRoles.MANAGER),
+  updateTicketValidation(),
   controllers.updateTicket
 ).delete(
-  verifyToken,
   authorizedRole(userRoles.ADMIN),
   singleTicketValidation(),
   controllers.deleteTicket
