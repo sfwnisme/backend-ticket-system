c328543759cf8d048c8b84c1b494602644ab75eb
sfwnisme <safymo424@gmail.com>
2025-04-29 01:37:15 +0300

    refactor(comment.model.js): remove required for author field

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -14,7 +14,7 @@ const commentSchema = new mongoose.Schema({
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    // required: true
   },
   isSolution: {
     type: Boolean,
