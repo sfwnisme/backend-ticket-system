36e54954b18004bc3f9e07aceab34989969aa29a
sfwnisme <safymo424@gmail.com>
2025-04-29 01:39:15 +0300

    refactor(comment.routes.js): apply verifyToken at the top and add the rest routes validations

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -6,15 +6,17 @@ const verifyToken = require('../middlewares/verifyToken');
 const authorizedRole = require('../middlewares/authorizedRole');
 const userRoles = require('../config/userRoles.config')
 
+router.use(verifyToken)
+
 router.route('/')
-  .get(verifyToken, authorizedRole(...Object.values(userRoles)), controllers.getAllComments)
+  .get(authorizedRole(...Object.values(userRoles)), controllers.getAllComments)
 
 router.route('/create')
   .post(createCommentValidation(), controllers.createComment)
 
 router.route('/:commentId')
-  .get(verifyToken, authorizedRole(...Object.values(userRoles)), singleCommentValidation(), controllers.getSingleComment)
-  .patch(verifyToken, authorizedRole(userRoles.ADMIN, userRoles.MANAGER, userRoles.CSR), updateCommentValidation(), controllers.updateComment)
-  .delete(verifyToken, authorizedRole(userRoles.ADMIN), singleCommentValidation(), controllers.deleteComment)
+  .get(authorizedRole(...Object.values(userRoles)), singleCommentValidation(), controllers.getSingleComment)
+  .patch(authorizedRole(userRoles.ADMIN, userRoles.MANAGER, userRoles.CSR), updateCommentValidation(), controllers.updateComment)
+  .delete(authorizedRole(userRoles.ADMIN), singleCommentValidation(), controllers.deleteComment)
 
 module.exports = router
