252011431c22cfbe4153ec408791ae3fe82fce93
sfwnisme <safymo424@gmail.com>
2025-05-21 00:33:06 +0300

    refactor(user.routes): comment delete users route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,27 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/user.controllers');
-const { registerValidation, loginValidation, updateUserValidation } = require('../middlewares/validationSchema');
+const { registerValidation,
+  loginValidation,
+  updateUserValidation } = require('../middlewares/validationSchema');
 const verifyToken = require('../middlewares/verifyToken');
 const authorizedRole = require('../middlewares/authorizedRole');
 const userRoles = require('../config/userRoles.config')
 
 router.route('/')
-  .get(verifyToken, authorizedRole(...Object.values(userRoles)), controllers.getAllUsers)
-  .delete(verifyToken, authorizedRole(userRoles.ADMIN), controllers.deleteUsers)
+  .get(
+    verifyToken,
+    authorizedRole(...Object.values(userRoles)),
+    controllers.getAllUsers
+  )
+// .delete(
+//   verifyToken,
+//   authorizedRole(userRoles.ADMIN),
+//   controllers.deleteUsers
+// )
 
 router.route('/me')
-  .get(verifyToken, controllers.getCurrentUser)
+  .get(
+    verifyToken,
+    controllers.getCurrentUser
+  )
 
 router.route('/register')
-  .post(registerValidation(), controllers.register)
+  .post(
+    verifyToken,
+    authorizedRole(userRoles.ADMIN),
+    registerValidation(),
+    controllers.register
+  )
 
 router.route('/login')
   .post(loginValidation(), controllers.login)
 
 router.route('/:userId')
-  .get(verifyToken, authorizedRole(userRoles.ADMIN, userRoles.MANAGER), controllers.getSingleUser)
-  .patch(verifyToken, authorizedRole(userRoles.ADMIN, userRoles.MANAGER), updateUserValidation(), controllers.updateUser)
-  .delete(verifyToken, authorizedRole(userRoles.ADMIN), controllers.deleteUser)
+  .get(
+    verifyToken,
+    authorizedRole(userRoles.ADMIN,
+      userRoles.MANAGER),
+    controllers.getSingleUser
+  )
+  .patch(
+    verifyToken,
+    authorizedRole(userRoles.ADMIN,
+      userRoles.MANAGER),
+    updateUserValidation(),
+    controllers.updateUser
+  )
+  .delete(
+    verifyToken,
+    authorizedRole(userRoles.ADMIN),
+    controllers.deleteUser
+  )
 
 module.exports = router
