53779ac31c0d6e3231d9f9d5cd734b1502a6ca3a
Safwan Mohamed <sfwn@Safwans-MacBook-Air.local>
2025-09-01 01:01:44 +0300

    refactor: change server port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 const app = require('./src/app')
 
 // Create the server instance
-const server = app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server working on port ${process.env.PORT || 5000}`);
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server working on port ${process.env.PORT || 8000}`);
 });
